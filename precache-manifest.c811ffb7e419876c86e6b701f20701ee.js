998188edb7219d20d6a8228b75a8c1c597343b70
Neli Kostadinova <nko@sitecore.net>
2020-03-25 12:12:48 +0200

    Updates

---
+++ precache-manifest.c811ffb7e419876c86e6b701f20701ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8e407e6c78cfb53bcb1042ec13b487",
+    "url": "/recipe-box/index.html"
+  },
+  {
+    "revision": "9dfce5901305516f199b",
+    "url": "/recipe-box/static/css/main.ed4f76d2.chunk.css"
+  },
+  {
+    "revision": "612c06fac98449141159",
+    "url": "/recipe-box/static/js/2.faa2945b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/recipe-box/static/js/2.faa2945b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dfce5901305516f199b",
+    "url": "/recipe-box/static/js/main.64b4f280.chunk.js"
+  },
+  {
+    "revision": "34b673932c5f5785b601",
+    "url": "/recipe-box/static/js/runtime-main.7648288e.js"
+  },
+  {
+    "revision": "9c5b9b751e82960c9012d846f91c708c",
+    "url": "/recipe-box/static/media/back.9c5b9b75.svg"
+  },
+  {
+    "revision": "989fd4e26e3957d76c9a19ee3e09caca",
+    "url": "/recipe-box/static/media/cart.989fd4e2.svg"
+  },
+  {
+    "revision": "52bd3043f2f7e6758d725443a317f433",
+    "url": "/recipe-box/static/media/plus.52bd3043.svg"
+  }
+]);
